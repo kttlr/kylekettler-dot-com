4165dc0150945646a8564ff11e4256036f49ac7a
Kyle Kettler <hello@kylekettler.com>
2023-10-24 15:46:28 -0500

    restarted with fresh astro install, built some basic layout component, started content collections

---
+++ astro/src/content/projects/designengineer.md
@@ -0,0 +1,8 @@
+---
+author: kyle-kettler
+categories: ["project"]
+date: 08/10/2023
+featured: false
+image: /images/designengineer.png
+title: Designengineer
+---
---
+++ astro/tailwind.config.mjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			maxWidth: {
+        152: '38rem',
+        164: '41rem',
+        208: '52rem',
+      },
+		},
+	},
+	plugins: [],
+}
