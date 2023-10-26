fe8ef66f4597eb0ee25034f141160787ed65db79
Kyle Kettler <hello@kylekettler.com>
2023-10-26 15:23:34 -0500

    WIP homepage

--- astro/tailwind.config.mjs
+++ astro/tailwind.config.mjs
@@ -8,6 +8,17 @@ export default {
         164: '41rem',
         208: '52rem',
       },
+			animation: {
+				'fade-in': 'fadeIn 1s ease-in',
+			},
+			"fade-in": {
+    		"0%": {
+      	"opacity": "0"
+    		},
+    		"100%": {
+    	  "opacity": "1"
+    		}
+		  },
 		},
 	},
 	plugins: [require('@tailwindcss/typography')],
